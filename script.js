7e02d31c077cfc5c74e5375386d11f8968215247
Hannah Kim <h.heeeun.kim@gmail.com>
2017-08-15 20:58:38 +0000

    finished script.js; takes in a single command line argument and use it to search in database

---
+++ script.js
@@ -0,0 +1,41 @@
+// console.log(process.argv.slice(2)); //array
+// console.log(process.argv[2]; //input
+
+const valueFromUser = process.argv[2];
+
+const pg = require("pg");
+const Pool = pg.Pool;
+// const Client = pg.Client;
+
+const settings = require("./settings"); // settings.json
+
+const pool = new Pool(settings);
+// const client = new pg.Client();
+
+pool.connect((err, client, done) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE last_name=$1 OR first_name=$1", [valueFromUser], (err, result) => {
+    done();
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log("Searching...");
+    const final = result.rows
+
+    console.log("Found", final.length, "person(s) by the name '", valueFromUser, "'")
+
+    final.forEach(function(info) {
+      console.log(info.id, ":", info.first_name, info.last_name, info.birthdate);
+    });
+
+  });
+});
+
+
+
+
+
+
+
